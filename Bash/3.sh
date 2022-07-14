a9ebe6db27abeacc73c6dffaa1d7244a14eb6acc
YuregLaz <yurkalazorenko@gmail.com>
2022-07-14 16:12:56 +0300

    mod: change 3.sh

--- Bash/3.sh
+++ Bash/3.sh
@@ -1,7 +1,7 @@
   GNU nano 6.2                                   /home/yureg/2.sh
 #!/bin/bash
 regex="^([a-zA-Z0-9]{1}[a-zA-Z0-9\.]{1,}[a-zA-Z0-9]{1}+)@([a-zA-Z0-9\.]+)\.([a-zA-Z]{2,5})$"
-file="/home/yureg/Lazorenko/Lazorenko.txt"
+filepath="/home/yureg/Lazorenko/Lazorenko.txt"
 email=$(cat "$file")
 echo $email
 if [[ $email =~ $regex ]] ; then
