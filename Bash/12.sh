0bbe73d65ac1ec343863ca157cd6cb1f595d13fa
YuregLaz <yurkalazorenko@gmail.com>
2022-07-14 16:49:49 +0300

    mod: change 3.sh

--- Bash/12.sh
+++ Bash/12.sh
@@ -3,13 +3,13 @@
 #lazorenko tree #12
 fn="Yuriy"
 ln="Lazorenko"
-gr1text="Summer2022 13/07/2022"
-gr2text="yurkalazorenko@gmail.com"
+text1="Summer2022 13/07/2022"
+text2="yurkalazorenko@gmail.com"
 mkdir -p $ln/{TAA,TBB,TDK}
 mkdir $ln/TBB/gr1
 mkdir $ln/TDK/gr2
-echo $gr1text>$ln/TBB/gr1/$fn.txt
-echo $gr2text>$ln/TDK/gr2/$ln.txt
+echo $text1>$ln/TBB/gr1/$fn.txt
+echo $text2>$ln/TDK/gr2/$ln.txt
 find ~/$ln -type f -execdir cp  "{}" ~/$ln ";"
 
 
